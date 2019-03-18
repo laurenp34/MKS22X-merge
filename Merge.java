f6a869eb141dc3065c39b60f725b89856c405b1c
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-03-18 10:03:27 -0400

    added wrapper function code, calling mergesort helper on each half

--- Merge.java
+++ Merge.java
@@ -2,7 +2,15 @@ public class Merge {
 
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[]data){
-    
+    if (data.length <= 1) return;
+    mergesort(data,0,(data.length/2)-1);
+    mergesort(data,(data.length/2),data.length-1);
+
+
+  }
+
+  public static void mergesort(int[] data, int lo, int hi) {
+
   }
 
 
